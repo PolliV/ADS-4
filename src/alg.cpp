c7bffd3bd4074591e5009f380bb1e08958417811
PolliV <77505747+PolliV@users.noreply.github.com>
2023-03-26 23:23:27 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,51 +1,50 @@
 // Copyright 2021 NNTU-CS
-int countPairs1(int *arr, int len, int value) {
-  int k = 0;
-  for (int i = 0; i < len; i++) {
-    for (int j = i + 1; j < len; i++) {
-      if (arr[i] + arr[j] == value) {
-        k += 1;
-      }
-    }
-  }
-  return k;
-}
-int countPairs2(int *arr, int len, int value) {
-  int k = 0;
+int countPairs1(int* arr, int len, int value) {
+    int k = 0;
     for (int i = 0; i < len - 1; i++) {
-        for (int j = len - 1; i < j; j--) {
-            if (arr[i] + arr[j] == value) {
-                k += 1;
+      for (int j = i + 1; j < len; j++) {
+        if (arr[i] + arr[j] == value) {
+            k += 1;
             }
         }
     }
     return k;
 }
-int countPairs3(int *arr, int len, int value) {
-  int k = 0;
-  for (int i = 0; i < len - 1; i++) {
-        int left = i;
-        int right = len;
-        while (left < right - 1) {
-            int middle = (left + right) / 2;
-            if (arr[i] + arr[middle] == value) {
-                k += 1;
-                int newmiddle = middle + 1;
-                while (arr[i] + arr[newmiddle] == value && newmiddle < right) {
-                    k += 1;
-                    newmiddle += 1;
+int countPairs2(int* arr, int len, int value) {
+    int k = 0;
+    for (int i = 0; i < len - 1; i++) {
+      for (int j = len - 1; i < j; j--) {
+        if (arr[i] + arr[j] == value) {
+            k += 1;
+            }
+        }
+    }
+    return k;
+}
+int countPairs3(int* arr, int len, int value) {
+    int k = 0;
+    for (int i = 0; i < len - 1; i++) {
+        int first = i, second = len;
+        while (first < second - 1) {
+            int m = (first + second) / 2;
+            if (arr[i] + arr[m] == value) {
+                k = k + 1;
+                int nm = m + 1;
+                while (arr[i] + arr[nm] == value && nm < second) {
+                    k = k + 1;
+                    nm = nm + 1;
                 }
-                newmiddle = middle - 1;
-                while (arr[i] + arr[newmiddle] == value && newmiddle > left) {
-                    k += 1;
-                    newmiddle -= 1;
+                nm = m - 1;
+                while (arr[i] + arr[nm] == value && nm > first) {
+                    k = k + 1;
+                    nm = nm - 1;
                 }
                 break;
             }
-            if (arr[i] + arr[middle] > value) {
-                right = middle;
+            if (arr[i] + arr[m] > value) {
+                second = m;
             } else {
-                left = middle;
+                first = m;
             }
         }
     }
